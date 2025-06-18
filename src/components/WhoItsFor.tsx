832edb666105bc755fd6c87d5b81a65d95940f6f
Philip-D21 <philiplekan88@gmail.com>
2025-06-18 22:38:59 +0100

    feat: first commit for sow

--- src/components/WhoItsFor.tsx
+++ src/components/WhoItsFor.tsx
@@ -21,13 +21,15 @@ const features = [
   },
 ]
 
+
 // Custom Checkmark Icon SVG Component
-const BlackCheckCircleIcon = () => (
-  <svg className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="12" fill="#1A1A1A"/>
-    <path d="M9 12L11 14L15 10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-)
+// const BlackCheckCircleIcon = () => (
+//   <svg className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <circle cx="12" cy="12" r="12" fill="#1A1A1A"/>
+//     <path d="M9 12L11 14L15 10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//   </svg>
+// )
+
 
 const WhoItsFor = () => {
   return (
